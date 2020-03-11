81f69acd6df8e73359ee7319bcfee4ddabc7c28e
lorenaadrian <lorenaadrian@gmail.com>
2020-03-10 21:55:18 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,19 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      { path: '', component: LoginComponent },
     ])
   ],
   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent
+    LoginComponent
   ],
   bootstrap: [ AppComponent ]
 })
